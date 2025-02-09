e63283f0b6b463b344c983285e447acb78e219cc
Nykyta_Vizniuk <nikitos.v.leo@gmail.com>
2025-02-09 12:14:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/notemaster/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.notemaster
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.notemaster.ui.theme.NoteMasterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NoteMasterTheme {
+                Surface {
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(55.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NoteMasterTheme {
+        Greeting("Android")
+    }
+}
